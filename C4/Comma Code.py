51397b4a1d7cfe4b460c8047e3104e65cef069a5
Yan Soum <romantic.night.dreamer@gmail.com>
2019-01-17 22:18:56 +0300

    finished
    i consider this finished as it functions as it was asked to. I added a function for checking if the items in a list are strings and if they are not they get converted into strings so that way it can be used with .join

--- C4/Comma Code.py
+++ C4/Comma Code.py
@@ -8,18 +8,25 @@ with and inserted before the last item. For example, passing the previous
 spam list to the function would return 'apples, bananas, tofu, and cats'.
 But your function should be able to work with any list value passed to it.'''
 
-dice_rolls = [3, 5, 6, 5, 5, 5, 6, 6, 5, 4, 5, 1, 2, 1, 1, 4, 6, 2, 3, 2]
+dr = dice_rolls = [3, 5, 6, 5, 5, 5, 6, 6, 5, 4, 5, 1, 2, 1, 1, 4, 6, 2, 3, 2]
 spam = ['apples', 'bananas', 'tofu', 'cats']
-mixed_list = [3, 5, 6, 5, 'apples', 'bananas', 2.2]
+ml = mixed_list = [3, 5, 6, 5, 'apples', 'bananas', 2.2]
 
 def lpas(list): #list_print_as_string
     seperator=", "
-    list_as_string = seperator.join(list[:-1])
+    list_as_string = seperator.join(ltc(list)[:-1])
     print("The list you provided contains")
-    print(list_as_string + " and " + list[-1])
+    print(list_as_string + " and " + str(list[-1]))
 
 
-#print(listlen(list)-1] + " and " list[-1])
-#print(list[:len(list)-1] + " and " list[-1])
+def ltc(list): #list_type_check
+    new_list = []
+    for item in list:
+        if type(item) != str:
+            new_list.append(str(item))
+        else:
+            new_list.append(item)
+    return new_list
 
-lpas(spam)
+
+lpas(ml)
