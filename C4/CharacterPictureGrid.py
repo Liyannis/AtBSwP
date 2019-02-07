7a317221a3054e62cafeeffbfe52b3bd6c9b5215
Yan Soum <romantic.night.dreamer@gmail.com>
2019-02-07 22:29:51 +0300

    .

--- C4/CharacterPictureGrid.py
+++ C4/CharacterPictureGrid.py
@@ -8,7 +8,11 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
 
-'''You can think of grid[x][y] as being the character at the x- and y-coordinates of a “picture” drawn with text characters. The (0, 0) origin will be in the upper-left corner, the x-coordinates increase going right, and the y-coordinates increase going down.
+'''You can think of grid[x][y] as being the character at the x- and y-coordinates
+of a “picture” drawn with text characters.
+The (0, 0) origin will be in the upper-left
+corner, the x-coordinates increase going right,
+and the y-coordinates increase going down.
 Copy the previous grid value, and write code that uses it to print the image.
 ..OO.OO..
 .OOOOOOO.
@@ -16,3 +20,22 @@ Copy the previous grid value, and write code that uses it to print the image.
 ..OOOOO..
 ...OOO...
 ....O....'''
+
+################################################################################
+# https://www.youtube.com/watch?v=wX7G6QLiPk8
+# https://youtu.be/XbWj0SkhWGo
+# https://youtu.be/uWBz_R8pNy4
+
+for x,y in grid([len(grid)], len(grid[1])):
+    print(len(x[0]))
+
+
+#def rotate_clockwise_90():
+#tempList = []
+#for x in grid:
+#    for y in grid:
+#        tempList.append(y)
+    
+
+
+#rotate_clockwise_90()
