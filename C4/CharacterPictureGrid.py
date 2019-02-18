cb21bfcb847e4ffcaab089c75ab472113aa051a5
Yan Soum <romantic.night.dreamer@gmail.com>
2019-02-19 00:19:43 +0300

    finally
    finished after headbutting for 3 weeks. With some help from uncle google :/

--- C4/CharacterPictureGrid.py
+++ C4/CharacterPictureGrid.py
@@ -1,4 +1,4 @@
-grid = [['.', '.', '.', '.', '.', '.'],
+grid = [['0', '.', '.', '.', '.', '5'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['O', 'O', 'O', 'O', '.', '.'],
         ['O', 'O', 'O', 'O', 'O', '.'],
@@ -6,7 +6,7 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['O', 'O', 'O', 'O', 'O', '.'],
         ['O', 'O', 'O', 'O', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
-        ['.', '.', '.', '.', '.', '.']]
+        ['8', '.', '.', '.', '.', '.']]
 
 '''You can think of grid[x][y] as being the character at the x- and y-coordinates
 of a “picture” drawn with text characters.
@@ -22,64 +22,11 @@ Copy the previous grid value, and write code that uses it to print the image.
 ....O....'''
 
 ################################################################################
-# https://www.youtube.com/watch?v=wX7G6QLiPk8
-# https://youtu.be/XbWj0SkhWGo
-# https://youtu.be/uWBz_R8pNy4
-
-for x,y in grid([len(grid)], len(grid[1])):
-    print(len(x[0]))
-
-
-#def rotate_clockwise_90():
-#tempList = []
-#for x in grid:
-#    for y in grid:
-#        tempList.append(y)
-    
-
-
-grid = [['0', '.', '.', '.', '.', '.'],
-        ['.', 'O', 'O', '.', '.', '.'],
-        ['O', 'O', 'O', 'O', '.', '.'],
-        ['O', 'O', 'O', 'O', 'O', '.'],
-        ['.', 'O', 'O', 'O', 'O', 'O'],
-        ['O', 'O', 'O', 'O', 'O', '.'],
-        ['O', 'O', 'O', 'O', '.', '.'],
-        ['.', 'O', 'O', '.', '.', '.'],
-        ['8', '.', '.', '.', '.', '.']]
-
-'''
-first i need to loop through grid[8-0]
-while in that loop i need to grab only the first item from the list
-append that to a temp list
-grid[8][0]
-grid[x][y]
-'''
-x = reversed(range(len(grid)))
-y = range(len(grid[0]))
-
-for p in x:
-  #print(grid[r])
-  for i in grid[p]:
-    print(i)
-
-  
-
-'''
-for li in reversed(range(len(grid))): #listindex
-    tl=[] #templist
-    for char in grid[li]:
-        #print(char)
-        tl.append(char)
-        print(tl)
-        
-    turnedGrid=[]
-    turnedGrid.append(tl)
-print(turnedGrid)
-'''
-# teat at home
-
 for char in range(len(grid[0])):
+    tempList = []
     for list in range(len(grid)):
-        print(char, list)
-        print(grid[list][char])
+        #print(char, list, grid[list][char])
+        tempList.append(grid[list][char])
+        if len(tempList) == len(grid):
+            tempListStr=''
+            print(tempListStr.join(tempList))
